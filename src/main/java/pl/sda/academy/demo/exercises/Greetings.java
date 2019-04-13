61f33bc7b994c80274e84805fa79adf976412859
Pawel Gruszczyk <pawel.gruszczyk96@gmail.com>
2019-04-13 16:02:01 +0200

    - many names

--- src/main/java/pl/sda/academy/demo/exercises/Greetings.java
+++ src/main/java/pl/sda/academy/demo/exercises/Greetings.java
@@ -7,12 +7,34 @@ public class Greetings {
         if (name == null) {
             return "Witaj, mój przyjacielu";
         } else {
-            if (name.toUpperCase().equals(name)) {
-                return "WITAJ, " + name + "!";
-            } else {
-                return "Witaj, " + name + "!";
+            String greeting = getGreetingForManyNames(name);
+            if (greeting != null) {
+                return greeting;
             }
 
+            return getGreetingForOneName(name);
         }
+    }
+
+    private String getGreetingForOneName(String name) {
+        if (name.toUpperCase().equals(name)) {
+            return "WITAJ, " + name + "!";
+        } else {
+            return "Witaj, " + name + "!";
+        }
+    }
+
+    private String getGreetingForManyNames(String name) {
+        String[] names = name.split(",");
+        int numberOfNames = names.length;
+        if (numberOfNames > 1) {
+            String greeting = names[0];
+            for (int i = 1; i < numberOfNames - 1; i++) {
+                greeting = greeting + ", " + names[i];
+            }
+            greeting = greeting + " i " + names[numberOfNames - 1] + ", witajcie!";
+            return greeting;
+        }
+        return null;
     }
 }
